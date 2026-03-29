0d4f47937e48de7ab0d9560b02a3fc27c4abf2f5
Surekha Yerra <96376069+Sindhu12lll@users.noreply.github.com>
2026-03-30 00:02:25 +0530

    Update database.sql

--- project/database.sql
+++ project/database.sql
@@ -1,1 +1,14 @@
-
+USE student_db;
+SELECT * FROM students;
+drop table students;
+CREATE TABLE students (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    course VARCHAR(100)
+);
+INSERT INTO students (name, age, course) VALUES ('Surekha', 21, 'Computer Science');
+INSERT INTO students (name, age, course) VALUES ('Ravi', 22, 'Electronics');
+INSERT INTO students (name, age, course) VALUES ('Priya', 20, 'Information Technology');
+INSERT INTO students (name, age, course) VALUES ('Kiran', 23, 'Mechanical');
+INSERT INTO students (name, age, course) VALUES ('Anjali', 21, 'Data Science');
